691d00fdc96114788580ce912cd0591a257dc1d3
Edgard Levy <edgardlevy@gmail.com>
2019-12-27 23:45:28 -0300

    styling menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import QRCode from 'react-native-qrcode-svg';
-import {Container, Code} from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {
+  Container,
+  Code,
+  Nav,
+  NavItem,
+  NavText,
+  SignOutButton,
+  SignOutButtonText,
+} from './styles';
 
 export default function Menu() {
   return (
@@ -13,6 +22,27 @@ export default function Menu() {
           backgroundColor="#8B10AE"
         />
       </Code>
+      <Nav>
+        <NavItem>
+          <Icon name="help-outline" size={20} color="#fff" />
+          <NavText>Me ajuda</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="person-outline" size={20} color="#fff" />
+          <NavText>Perfil</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="credit-card" size={20} color="#fff" />
+          <NavText>Configurar cartão</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="smartphone" size={20} color="#fff" />
+          <NavText>Configurações do app</NavText>
+        </NavItem>
+      </Nav>
+      <SignOutButton onPress={() => {}}>
+        <SignOutButtonText>SAIR DA CONTA</SignOutButtonText>
+      </SignOutButton>
     </Container>
   );
 }
